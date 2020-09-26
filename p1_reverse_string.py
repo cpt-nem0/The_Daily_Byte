1e6753398e66bc887e24658af06476f1ffef2360
cpt-nem0 <rohanya.76426@gmail.com>
2020-09-26 22:03:36 +0530

    comment added

--- p1_reverse_string.py
+++ p1_reverse_string.py
@@ -1,3 +1,13 @@
+"""
+Given a string, reverse all of its characters and return the resulting string.
+
+Ex: Given the following strings...
+“Cat”, return “taC”
+“The Daily Byte”, return "etyB yliaD ehT”
+“civic”, return “civic”
+"""
+
+
 def reverse_string():
     print(input()[::-1])
 
