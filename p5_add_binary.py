87db43b9915ebb5b6e9169aa55f96f16c9fd8be7
cpt-nem0 <rohanya.76426@gmail.com>
2020-09-29 01:18:28 +0530

    added p5

---
+++ p5_add_binary.py
@@ -0,0 +1,48 @@
+"""
+Given two binary strings (strings containing only 1s and 0s) return their sum (also as a binary string).
+Note: neither binary string will contain leading 0s unless the string itself is 0.
+
+Ex: Given the following binary strings...
+
+"100" + "1", return "101"
+"11" + "1", return "100"
+"1" + "0", return  "1"
+"""
+
+
+def addBinary(a: str, b: str)->str:
+    i = len(a)-1
+    j = len(b)-1
+    carry = 0
+    result = ''
+    
+    while i>=0 or j>=0:
+        sum = carry
+        
+        if(i>=0):
+            sum += ord(a[i]) - ord['0']
+        if(j>=0):
+            sum += ord(b[j]) - ord['1']
+        
+        carry = 1 if sum%2==0 else 0
+        
+        result += str(sum%2)
+        
+        if carry:
+            result += str(carry)
+
+        return result[::-1]
+
+
+if __name__ == "__main__":
+    a = input('a> ')
+    b = input('b> ')
+
+    addBinary(a, b)
+    
+    
+    
+    
+# Alternative: 
+# def addBinary(a: str, b: str)->str:   
+#   return str(bin(int(a,2)+int(b,2)))[2:]
